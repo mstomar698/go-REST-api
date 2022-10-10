37545b698f51de5227fe506c2ed8aff7ce4c2cb8
201b153 <tomarm698@gmail.com>
2022-10-11 00:43:55 +0530

    first endpoint

--- main/main.go
+++ main/main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	// "encoding/json"
 	"github.com/gorilla/mux"
+	"github.com/mstomar698/go-REST-api/package/handlers"
 )
 
 func main() {
@@ -14,6 +15,7 @@ func main() {
 	// router.HandleFunc("/books", func(w http.ResponseWriter, r *http.Request) {
 	// 	json.NewEncoder(w).Encode("Hello World")
 	// })
+	router.HandleFunc("/books", handlers.GetAllBooks).Methods(http.MethodGet)
 
 	log.Println("Api is running on port 4000")
 	http.ListenAndServe(":4000", router)
---
+++ package/mocks/book.go
@@ -0,0 +1,13 @@
+// mock db
+package mocks
+
+import  "github.com/mstomar698/go-REST-api/package/models"
+
+var Books = []models.Book{
+	{
+		Id: 1,
+		Title: "js",
+		Author: "not u",
+		Desc: "try it",
+	},
+}
